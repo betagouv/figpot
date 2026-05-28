9d2f5fa21cf8863d5a04636c2a852120fd0ab9c7
Thomas Ramé <thomas@rame.fr>
2026-05-28 23:33:52 +0200

    fix(transform): line height in figma can be set to 0

--- src/features/translators/text/properties/translateLineHeight.ts
+++ src/features/translators/text/properties/translateLineHeight.ts
@@ -2,8 +2,8 @@ import { TypeStyle } from '@figpot/src/clients/figma';
 import { workaroundAssert as assert } from '@figpot/src/utils/assert';
 
 export function translateLineHeight(segment: Pick<TypeStyle, 'lineHeightPx' | 'lineHeightPercentFontSize' | 'lineHeightUnit' | 'fontSize'>): string {
-  assert(segment.fontSize);
-  assert(segment.lineHeightPx);
+  assert(segment.fontSize !== undefined && segment.fontSize > 0); // If setting 0 in Figma is automatically goes to 1px
+  assert(segment.lineHeightPx !== undefined);
 
   // Penpot's line height is a unitless ratio of the font size. Figma always exposes the resolved line height
   // in pixels (`lineHeightPx`), whatever unit it was authored with (`PIXELS`, `FONT_SIZE_%`, `INTRINSIC_%`),
